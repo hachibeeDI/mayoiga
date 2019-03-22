2ed4c0fa9ef59d9d794f0a06bde0d8124ea218ee
OguraDaiki <8hachibee125@gmail.com>
2019-03-22 16:59:00 +0900

    support validation idea

--- src/index.tsx
+++ src/index.tsx
@@ -8,7 +8,7 @@ import {Form, Field, connectForm} from './forms';
 export {Form, Field, connectForm};
 export default {Form, Field, connectForm};
 
-type FormReducerActionTypes = 'CHANGE';
+type FormReducerActionTypes = 'CHANGE' | 'SUBMIT';
 type FSA<S> = {
   type: FormReducerActionTypes;
   payload: {
@@ -22,19 +22,21 @@ type MayoigaContextValue<S> = {
   dispatch: Dispatch<FSA<S>>;
 };
 
+type Validator<S, Name extends keyof S> = (target: S[Name]) => undefined | string;
+
 type FormEffect<S> = {
   getState(): S;
 };
 
 type FormProps<S> = {
   onSubmit(value: S): void;
-  mapChanged(value: S): S;
   children: ReactNode;
 };
 
 type FieldProps<S, Name extends keyof S> = {
   name: Name;
   onChange?(name: Name, value: S[Name]): void;
+  validations?: ReadonlyArray<Validator<S, Name>>;
   // children(props: {value: S[Name]; onChange?(e: SyntheticEvent<unknown>): void}): ReactNode;
 };
 
@@ -48,16 +50,25 @@ function fieldChange<S>(name: keyof S, value: S[keyof S]): FSA<S> {
   };
 }
 
+function submitValue<S>(state: S): FSA<S> {
+  return {
+    type: 'SUBMIT',
+    payload: {} as any, // FIXME: I know this is wrong but it's mendokusai. Fix FSA later.
+  };
+}
+
 type MayoigaProps<S> = {
   initialState: S;
+  onSubmit(value: S): void;
 };
 
+// TODO: need `mapChanged(value: S): S;` ?
 export function createFormScope<S>() {
   const Ctx = createContext((null as any) as MayoigaContextValue<S>);
 
   return {
     context: Ctx,
-    scope: function<OwnProps>(ConnectedComponent: FC<OwnProps>) {
+    scope: function<OwnProps = {}>(ConnectedComponent: FC<OwnProps>) {
       return (props: OwnProps & MayoigaProps<S>) => {
         const {initialState} = props;
         const [state, dispatch] = useReducer((state: S, action: FSA<S>) => {
@@ -65,6 +76,11 @@ export function createFormScope<S>() {
             case 'CHANGE': {
               const {name, value} = action.payload;
               return {...state, [name]: value};
+            }
+            case 'SUBMIT': {
+              // I should repent my sin. I called side effect in reducer...
+              props.onSubmit(state);
+              return state;
             }
             default:
               return state;
@@ -85,12 +101,16 @@ export function useForm<S>(formScope: Context<MayoigaContextValue<S>>) {
   return useMemo(
     () => ({
       Form: (props: FormProps<S>) => {
-        const {state} = useContext(formScope);
+        const {state, dispatch} = useContext(formScope);
+        const {onSubmit} = props;
         return (
           <form
             onSubmit={e => {
               e.preventDefault();
-              props.onSubmit(state);
+              if (onSubmit) {
+                onSubmit(state);
+              }
+              dispatch(submitValue(state));
             }}
           >
             {props.children}
@@ -99,7 +119,7 @@ export function useForm<S>(formScope: Context<MayoigaContextValue<S>>) {
       },
       Field: <Name extends keyof S>(props: FieldProps<S, Name>) => {
         const {state, dispatch} = useContext(formScope);
-        const {name, onChange} = props;
+        const {name, validations, onChange} = props;
         const value = state[name];
 
         const handleChange = useCallback(
@@ -113,9 +133,15 @@ export function useForm<S>(formScope: Context<MayoigaContextValue<S>>) {
           [name, state]
         );
 
+        let err: string | undefined;
+        if (validations !== undefined) {
+          err = validations.map(v => v(value)).find(result => !!result);
+        }
+
         return (
           <>
             <input key={name as string} name={name as string} value={value.toString()} onChange={handleChange} />
+            {err && <div style={{color: 'red'}}>{err}</div>}
           </>
         );
       },
