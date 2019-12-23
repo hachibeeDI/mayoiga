79666238964574531bc6b136d23e4fbd070a1adf
OguraDaiki <8hachibee125@gmail.com>
2019-12-23 16:28:08 +0900

    remove unused imports

--- src/inputProtocol.tsx
+++ src/inputProtocol.tsx
@@ -1,6 +1,4 @@
-import * as React from 'react';
-
-import {useCallback, InputHTMLAttributes, SyntheticEvent} from 'react';
+import {InputHTMLAttributes} from 'react';
 
 type Omit<T, K extends keyof T> = Pick<T, Exclude<keyof T, K>>;
 
