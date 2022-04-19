4373bee5a119f1c7c2e8f6084662b5c5ab502594
Ogura Daiki <8hachibee125@gmail.com>
2022-04-19 16:59:46 +0900

    use setup file

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,8 @@ import {defineConfig} from 'vitest/config';
 
 export default defineConfig({
   test: {
+    globals: true,
     environment: 'happy-dom',
+    setupFiles: ['./setup/vite-global-setup.js'],
   },
 })
